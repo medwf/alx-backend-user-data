97016e43dc0b3b6c512a4623d8d1dd264402c7a7
med.wf <med.wf95@gmail.com>
2024-05-30 11:27:22 +0100

    add task 0 - delete some docummentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,20 +10,7 @@ def filter_datum(
         fields: List[str], redaction: str,
         message: str, separator: str
 ) -> str:
-    """
-    Method That returns:
-        The log message obfuscated
-
-    Args:
-        fields <list[str]>: All fields to obfuscate
-        redaction <str>: what the field will be obfuscated
-        message <str>: The log line
-        separator <str>: which character is separating
-
-    Return:
-        The log message obfuscated.
-    """
-    # for field in fields:
+    """The log message obfuscated"""
     allFields = r"(?P<fields>{})=[^{}]*".format('|'.join(fields), separator)
     replace = r"\g<fields>={}".format(redaction)
     return sub(allFields, replace, message)
