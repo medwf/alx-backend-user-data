c453919841fefeba38cea2539eb060a8098f9850
med.wf <med.wf95@gmail.com>
2024-05-30 12:20:03 +0100

    add task 1 - filter incomming log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ module personnel data.
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(
@@ -13,3 +14,25 @@ def filter_datum(
     allFields = r"(?P<fields>{})=[^{}]*".format('|'.join(fields), separator)
     replace = r"\g<fields>={}".format(redaction)
     return re.sub(allFields, replace, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """init instance"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values incoming log records"""
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+            self.fields, self.REDACTION,
+            message, self.SEPARATOR
+        )
