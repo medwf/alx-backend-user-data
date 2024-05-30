da45fc478f920223779e01c3356a12f7ba600441
med.wf <med.wf95@gmail.com>
2024-05-30 12:48:40 +0100

    add task 1 - annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,7 +24,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """init instance"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
