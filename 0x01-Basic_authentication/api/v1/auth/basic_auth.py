da78ad20e9f36b604ee08d4cbb3e23fccc7acb02
med.wf <med.wf95@gmail.com>
2024-06-03 21:56:07 +0100

    add new methods user_object_from_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """this module for basic authentication"""
 from api.v1.auth.auth import Auth
 from base64 import b64decode, decode
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -52,3 +53,20 @@ class BasicAuth(Auth):
                 ":" not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """The User instance based on his email and password."""
+        if not user_email or \
+                not isinstance(user_email, str) or \
+                not user_pwd or \
+                not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+            if len(users) and users[0].is_valid_password(user_pwd):
+                return users[0]
+        except Exception:
+            pass
+        return None
