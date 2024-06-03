1890fbf28ad116c260eb53ff0f95f78d1e3107ad
med.wf <med.wf95@gmail.com>
2024-06-03 18:34:32 +0100

    add methods extract_user_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """this module for basic authentication"""
 from api.v1.auth.auth import Auth
 from base64 import b64decode, decode
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -38,3 +39,16 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return data_bytes.decode('utf-8')
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> Tuple[str, str]:
+        """Return:
+            The user email and password
+            from the Base64 decoded value.
+        """
+        if not decoded_base64_authorization_header or\
+                not isinstance(decoded_base64_authorization_header, str) or\
+                ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(":"))
