56bee76ba2bce7973c0b6a7678c920a300137cca
med.wf <med.wf95@gmail.com>
2024-06-05 18:02:52 +0100

    yes i did it just a pycodestyle 😎✌️

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -31,7 +31,8 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout():
     """logout session"""
     from api.v1.app import auth
