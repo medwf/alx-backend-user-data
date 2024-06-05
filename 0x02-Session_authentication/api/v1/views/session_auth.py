69b4ae336fbea629f130ebf961afea1466ac0029
med.wf <med.wf95@gmail.com>
2024-06-05 17:07:41 +0100

    handles routes for the Session authentication.

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""handles all routes for the Session authentication"""
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """login session"""
+    from api.v1.app import auth
+    from os import getenv
+    email = request.form.get('email', '')
+    if len(email) == 0:
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password', '')
+    if len(password) == 0:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+    if not len(user):
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    # print(f"\033[32m{getenv('SESSION_NAME')}:{session_id}\033[0m")
+    response.set_cookie(getenv('SESSION_NAME'), session_id)
+    return response
