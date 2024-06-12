c8841df8f81a579f0f6b9b6b47097d3548d2909a
med.wf <med.wf95@gmail.com>
2024-06-12 19:12:02 +0100

    add update password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,15 @@ class Auth:
             raise ValueError
         self._db.update_user(user.id, reset_token=_generate_uuid())
         return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except Exception:
+            raise ValueError
+        self._db.update_user(
+            user.id,
+            hashed_password=_hash_password(password),
+            reset_token=None
+        )
