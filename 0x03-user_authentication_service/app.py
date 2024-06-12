fb5bec71b7e3c28464c7320fc6ecaa00722552a1
med.wf <med.wf95@gmail.com>
2024-06-12 19:02:04 +0100

    add reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,18 @@ def profile():
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def reset_password():
+    """Reset password"""
+    email = request.form.get('email', None)
+    if email:
+        try:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": email, "reset_token": token}), 200
+        except ValueError:
+            pass
+    abort(403)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
