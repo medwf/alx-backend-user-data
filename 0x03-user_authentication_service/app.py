d69562cd55b4a84f7e1d78dabceacb6c961056ae
med.wf <med.wf95@gmail.com>
2024-06-12 12:01:33 +0100

    add user methods with POST

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 Create module basic Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -13,5 +15,19 @@ def hello() -> str:
     return jsonify({'message': 'Bienvenue'}), 200
 
 
+@app.route('/users', methods=['POST'])
+def users():
+    """create a user with post methods"""
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    if email and password:
+        try:
+            AUTH.register_user(email, password)
+        except ValueError:
+            return jsonify({'message': 'email already registered'}), 400
+        return jsonify({'email': email, 'message': 'user created'}), 200
+    return jsonify({'message': 'should have email and password'})
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
